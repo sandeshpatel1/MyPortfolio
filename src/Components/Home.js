7f29e3ddb3e762aa37ea5f8431d60693e14081b6
PATEL SANDESH <patelsandesh1@gmail.com>
2024-02-29 22:11:30 +0530

    Responsive Styling fixed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "../Components/Home.css";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
